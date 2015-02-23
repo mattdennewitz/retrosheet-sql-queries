06fbc418fa82d82bd953f70548a1ddffe26dc929
Matt Dennewitz <mattdennewitz@gmail.com>
2015-02-23 12:45:42 -0600

    Removed "year_id" from Complete Innings query

--- complete-innings/complete-innings.sql
+++ complete-innings/complete-innings.sql
@@ -1,8 +1,8 @@
-create table non_partial_non_home_half_ninth_plus_innings
+create table
+    non_partial_non_home_half_ninth_plus_innings
 as
 select
-    year_id
-    , game_id
+    game_id
     , inn_ct
     , bat_home_id
 from
@@ -10,4 +10,5 @@ from
 where
     inn_end_fl = 'T'
     and event_outs_ct + outs_ct = 3
-    and (case when inn_ct = 9 and bat_home_id::numeric = 1 then 1 else 0 end) = 0;
+    and (case when inn_ct = 9 and bat_home_id::numeric = 1 then 1 else 0 end) = 0
+;
